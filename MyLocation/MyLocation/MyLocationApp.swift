a58e6e918f933bf575ac3e42b27e148b6c977d7d
Sarvar019 <megafon.kcr.09.09@gmail.com>
2025-01-20 18:31:08 +0500

    create xcode project

---
+++ MyLocation/MyLocation/MyLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLocationApp.swift
+//  MyLocation
+//
+//  Created by Sarvar Boltaboyev on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MyLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
